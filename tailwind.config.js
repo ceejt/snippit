3a7d238f0c5a0ba59672f9a2b85f2e622c3b7ca5
Cyril Josef Tinae <cyriljoseft@gmail.com>
2025-11-28 01:13:05 +0800

    Updated tailwind config with keyframes and animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
       spacing: {
         128: "32rem",
       },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        spin: "spin 1s linear infinite",
+      },
     },
   },
   plugins: [],
